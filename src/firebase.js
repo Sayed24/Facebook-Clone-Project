ddad055bda870bcdf22abc6faf0b78dff98c8da1
Sayed Sadat <SadatSayed92@gmail.com>
2020-11-05 12:03:28 -0800

    Facebook Clone

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD6wgNarVBMvCxXbusVqBfP_T1BP1ydTwM",
+    authDomain: "facebookclonesadat.firebaseapp.com",
+    databaseURL: "https://facebookclonesadat.firebaseio.com",
+    projectId: "facebookclonesadat",
+    storageBucket: "facebookclonesadat.appspot.com",
+    messagingSenderId: "410308095173",
+    appId: "1:410308095173:web:dce8094bd40a2de46214ac",
+    measurementId: "G-7PFSP9LHN3"
+  };
