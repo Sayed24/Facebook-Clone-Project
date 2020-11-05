8bf61d3a59d4c9f820bc720171da22836882ffc7
Sayed Sadat <SadatSayed92@gmail.com>
2020-11-05 14:50:02 -0800

    Left Sidear

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import './App.css';
 import Header from './Header';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="app">
       <Header />
-      {/* App Body */}
-      {/* Sidebar */}
-      {/* Feedd */}
-      {/* Widgets */}
+      <div className="app__body">
+        <Sidebar />
+
+        {/* Feedd */}
+        {/* Widgets */}
+      </div>
     </div>
   );
 }
