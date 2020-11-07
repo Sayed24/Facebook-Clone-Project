082f31bae2526025232c1a84ca98f59316e3f27d
Sayed Sadat <SadatSayed92@gmail.com>
2020-11-06 18:42:50 -0800

    updates

--- src/Feed.js
+++ src/Feed.js
@@ -14,7 +14,7 @@ function Feed() {
                 profilePic="https://scontent.fsac1-1.fna.fbcdn.net/v/t31.0-1/p200x200/26910252_703819823154121_6652453779003603140_o.jpg?_nc_cat=111&ccb=2&_nc_sid=7206a8&_nc_ohc=P6mpHHKXF18AX9hKYuQ&_nc_oc=AQkpN8P8OBxCd-8DitgE4X2UVmawQLLX5-vd2IgTGP16GQWskQes6ifCv0jR4GHOSIP7w4h62zpi2AewXJOA7bW1&_nc_ht=scontent.fsac1-1.fna&tp=6&oh=8707be421857777f6c26885d9664f021&oe=5FC94D5D"
                 message="Ohhh, it looks pretty nice!"
                 timestamp="Here is the timestamp"
-                username="Sayed Sadat"
+                username="SayedRahim Sadat"
                 image="https://scontent.fsac1-1.fna.fbcdn.net/v/t1.0-9/51775664_916542131881888_5353898251697258496_o.jpg?_nc_cat=111&ccb=2&_nc_sid=8bfeb9&_nc_ohc=aoZ1vTmxYWYAX_63HtZ&_nc_ht=scontent.fsac1-1.fna&oh=5db5bed28511fee01152894d9b55ed3d&oe=5FCB3E93"
             />
 
